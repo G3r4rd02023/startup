ef147fabb2ecdc146ea5f3be04acfba8327e93c6
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-02-17 14:40:48 -0600

    Update Bootstrap

---
+++ startup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(startup.Startup))]
+namespace startup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
